14bb7492a4061569e506cdc4c3e22839c3708d70
shivanshu nigam <shivanshuN@200oksolutions.com>
2025-10-20 12:45:52 +0530

    asdsdasd

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use(
       "http://127.0.0.1:5173",
       "https://zentrover.com",
       "https://www.zentroverse.com",
+      
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
